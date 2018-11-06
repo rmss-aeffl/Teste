7ffda9718b29fcfcf1ca2b892f715ec34cd64604
Rui Soares <rui.soares.aeffl@hotmail.com>
2018-11-06 14:02:43 +0000

    laste version changed blue

---
+++ DB teste/DB teste/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DB_teste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+      
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            db_user a = new db_user();
+
+            if (a.validate_user_txt(textBox1.Text, textBox2.Text))
+                // abrir outro form
+                MessageBox.Show("Ok open new form");
+
+            else
+                MessageBox.Show("Erro pass ou user nao validos");
+        }
+    }
+}
---
+++ DB teste/DB teste/db_user.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_teste
+{
+    class db_user
+    {
+        string username;
+        string password;
+
+        public db_user()
+        {
+
+        }
+
+        public bool validate_user(string user, string pass) {
+
+      
+        }
+
+        public bool validate_user_txt(string user, string pass) {
+
+        }
+    }
+}
